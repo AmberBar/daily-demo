999a639b8433bbe59f1b55c5da6587b26c5563f6
amber.lei <amber.lei@useepay.com>
2022-04-14 08:31:28 +0800

    add sentinel simple demo

---
+++ sentinel-demo/README.md
@@ -0,0 +1,12 @@
+# 文档
+[Sentinel简单demo](https://github.com/alibaba/Sentinel/wiki/%E6%96%B0%E6%89%8B%E6%8C%87%E5%8D%97#%E5%85%AC%E7%BD%91-demo)
+
+## 不用框架方式
+### 引入maven
+```xml
+<dependency>
+    <groupId>com.alibaba.csp</groupId>
+    <artifactId>sentinel-core</artifactId>
+    <version>1.8.4</version>
+</dependency>
+```
---
+++ sentinel-demo/sentinel-simple/src/main/java/com/amber/sentienl/simple/SimpleDemo.java
@@ -0,0 +1,51 @@
+package com.amber.sentienl.simple;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 使用非注解方式
+ * @Author : Amber
+ * @create 2022-04-14 7:40
+ */
+public class SimpleDemo {
+    public static void main(String[] args) {
+        // 配置规则.
+        initFlowRules();
+
+        while (true) {
+            // 1.5.0 版本开始可以直接利用 try-with-resources 特性，自动 exit entry
+            try (Entry entry = SphU.entry("HelloWorld")) {
+                // 被保护的逻辑,这里一般写业务逻辑
+                System.out.println("hello world");
+            } catch (BlockException ex) {
+                // 处理被流控的逻辑
+                System.out.println("blocked!");
+                return;
+            } catch (Exception e) {
+                // 这里一般是自己的业务异常
+            }
+        }
+    }
+
+    /**
+     * 注册规则
+     */
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(2);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
